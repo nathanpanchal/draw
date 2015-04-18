1c65583f2eacab1a08f575143eef47fd602ea132
Nathan Panchal <nathanpanchal@gmail.com>
2015-04-17 17:01:12 -0700

    np - remove document ready method

--- js/script.js
+++ js/script.js
@@ -1,11 +1,8 @@
-$(document).ready(function () {
-    for (var i = 0; i < 256; i++) {
-        var $newdiv = $('<div>', {"class": "unit", text: ""});
-        $('.container').append($newdiv);
-    }
+for (var i = 0; i < 256; i++) {
+    var $newdiv = $('<div>', {"class": "unit", text: ""});
+    $('.container').append($newdiv);
+}
 
-    $('.unit').hover(function(){
-        $(this).addClass('hover');
-    });
+$('.unit').hover(function(){
+    $(this).addClass('hover');
 });
-

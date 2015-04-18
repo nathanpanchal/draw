17e7f70e7f95ebb7a308847549945b7398e63df2
Nathan Panchal <nathanpanchal@gmail.com>
2015-04-18 08:31:53 -0700

    np - found bug that slowed the program

--- js/script.js
+++ js/script.js
@@ -1,22 +1,22 @@
 function createGrid(sideLength) {
-    var gridSize = (sideLength * sideLength);
-    for (var i = 0; i < gridSize; i++) {
-      var $newdiv = $('<div>', {"class": "unit", text: ""});
-      $('.container').append($newdiv);
+  var gridSize = (sideLength * sideLength);
+  for (var i = 0; i < gridSize; i++) {
+    var $newdiv = $('<div>', {"class": "unit", text: ""});
+    $('.container').append($newdiv);
   };
   $('.unit').hover(function(){
-      $(this).addClass('hover');
+    $(this).addClass('hover');
   });
 };
 
 createGrid(16)
 
-$('#clear').click(function(){
-    var sideLength = prompt("A canvas is a square. What side length would you like?", 16);
+$('#button').click(function(){
+  var sideLength = prompt("A canvas is a square. What side length would you like?", 16);
 
-    if (sideLength != null) {
-        $('.container').css('width', (sideLength * 20));
-        $('.container').empty();
-        createGrid(sideLength);
-    };
+  if (sideLength != null) {
+    $('.container').css('width', (sideLength * 20));
+    $('.container').empty();
+    createGrid(sideLength);
+  };
 });

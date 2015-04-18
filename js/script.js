28ef640dcc11f4bbca199355f99f503b51b28c7e
Nathan Panchal <nathanpanchal@gmail.com>
2015-04-17 17:00:08 -0700

    np - initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    for (var i = 0; i < 256; i++) {
+        var $newdiv = $('<div>', {"class": "unit", text: ""});
+        $('.container').append($newdiv);
+    }
+
+    $('.unit').hover(function(){
+        $(this).addClass('hover');
+    });
+});
+
